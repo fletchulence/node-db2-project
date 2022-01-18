30ba8d88e92ced41a3f20406a575176ccd404649
david fletcher <davidfletcheraaron@gmail.com>
2022-01-18 13:26:27 -0500

    more initial things

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,7 @@
 // no need to change this file
 const knex = require('knex')
 const configs = require('../knexfile.js')
+//on heroku this will be whatever heroku needs to make this possible
 const environment = process.env.NODE_ENV || 'development'
 
 module.exports = knex(configs[environment])
