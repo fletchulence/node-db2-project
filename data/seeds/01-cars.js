0119a5e295aba31503a50e1f15eb197a36d14e0f
david fletcher <davidfletcheraaron@gmail.com>
2022-01-18 13:54:13 -0500

    seed file here

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,30 +1,35 @@
 // STRETCH
-exports.cars = [
 
-   {
-      car_id: 1,
-      car_vin: '1GCDC14H5DS161081',
-      car_make: '1983 Chevrolet',
-      car_model: '10 series',
-      car_mileage: 10000,
-      car_title: 'C K',
-      car_transmission: 'auto'
-   },
-   {
-      car_id: 2,
-      car_vin: 'JH4KA7556PC018027',
-      car_make: '1993 Acura',
-      car_model: 'Legend',
-      car_mileage: 10000,
-      car_title: 'C K',
-      car_transmission: 'auto'
-   },
-   {
-      car_id: 3,
-      car_vin: '3G4AG54N9NS614902',
-      car_make: '1992 Buick',
-      car_model: 'Century',
-      car_mileage: 200000,
-      car_transmission: 'auto'
-   },
-]
+exports.seed = function (knex) {
+   return knex('cars').del()
+   .then(function () {
+      return knex('cars').insert([
+          {
+             car_id: 1,
+             car_vin: '1GCDC14H5DS161081',
+             car_make: '1983 Chevrolet',
+             car_model: '10 series',
+             car_mileage: 104000,
+             car_title: 'C K',
+             car_transmission: 'auto'
+          },
+          {
+             car_id: 2,
+             car_vin: 'JH4KA7556PC018027',
+             car_make: '1993 Acura',
+             car_model: 'Legend',
+             car_mileage: 100900,
+             car_title: 'C K',
+             car_transmission: 'auto'
+          },
+          {
+             car_id: 3,
+             car_vin: '3G4AG54N9NS614902',
+             car_make: '1992 Buick',
+             car_model: 'Century',
+             car_mileage: 210000,
+             car_transmission: 'auto'
+          },
+       ]);
+     });
+ };
