aa6755fc8a029a2d69204a2f5fe0a348c1401ffd
david fletcher <davidfletcheraaron@gmail.com>
2022-01-18 13:27:14 -0500

    migration and  and seed made for car

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
-exports.up = function (knex) {
+exports.up = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.createTable('cars', tbl =>{
+    tbl.increments('car_id')
+    tbl.string('car_vin').notNullable().unique()
+    tbl.string('car_make').notNullable()
+    tbl.string('car_model').notNullable()
+    tbl.integer('car_mileage').notNullable()
+    tbl.string('car_title')
+    tbl.string('car_transmission')
+  })
 };
 
-exports.down = function (knex) {
+exports.down = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.dropTableIfExists('cars')
 };
