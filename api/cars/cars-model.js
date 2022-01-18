cb5a1fc4b106d40fb832f591035d9c24f71dd60e
david fletcher <davidfletcheraaron@gmail.com>
2022-01-18 15:10:18 -0500

    model is all hooked -- shouldnt do it that way...

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,28 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars')
+    .select('*')
 }
 
-const getById = () => {
+const getById = id => {
   // DO YOUR MAGIC
+  return db('cars')
+    .where({ id })
+    .first()
 }
 
-const create = () => {
+const create = async (car) => {
   // DO YOUR MAGIC
+  const [id] = await db('cars').insert( car )
+  return getById( id )
+    // .select('*')
+}
+
+
+module.exports = {
+  getAll,
+  getById,
+  create,
 }
