aa600933e60a8a280b9bd4c5e4ac5ea298acf07d
david fletcher <davidfletcheraaron@gmail.com>
2022-01-18 13:26:06 -0500

    dotenv must be installed first

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const server = require('./api/server')
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9004;
 
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
