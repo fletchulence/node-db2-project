8ef6f749e820eda660aaf1407cfc3ccb7f372634
david fletcher <davidfletcheraaron@gmail.com>
2022-01-18 13:56:38 -0500

    got rid of the car_id in my seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -5,7 +5,7 @@ exports.seed = function (knex) {
    .then(function () {
       return knex('cars').insert([
           {
-             car_id: 1,
+            //  car_id: 1,
              car_vin: '1GCDC14H5DS161081',
              car_make: '1983 Chevrolet',
              car_model: '10 series',
@@ -14,7 +14,7 @@ exports.seed = function (knex) {
              car_transmission: 'auto'
           },
           {
-             car_id: 2,
+            //  car_id: 2,
              car_vin: 'JH4KA7556PC018027',
              car_make: '1993 Acura',
              car_model: 'Legend',
@@ -23,7 +23,7 @@ exports.seed = function (knex) {
              car_transmission: 'auto'
           },
           {
-             car_id: 3,
+            //  car_id: 3,
              car_vin: '3G4AG54N9NS614902',
              car_make: '1992 Buick',
              car_model: 'Century',
